180c2340991cf6238b18d0bdff530b0e9238be27
Stefano De Colli <sdecolli@gmail.com>
2014-11-26 17:02:46 -0430

    Creates target and data

--- Proyecto Final/my_sarcasm_is_great.py
+++ Proyecto Final/my_sarcasm_is_great.py
@@ -3,5 +3,17 @@
 import csv
 with open('data.csv', 'r') as my_file:
 	my_csv = csv.reader(my_file)
+
+	# En el data set el target esta en la columna 2 y el resto
+	# de los atributos en las columnas > 2
+	target = []
+	data = []
+
+	# Saltamos el header
+	next(my_csv)
+
+	# Llenamos las tuplas de target (Valores esperados)
+	# y las de data (Valores que tenemos)
 	for row in my_csv:
-		print(row)
+		target.append(row[1])
+		data.append(row[2:])
