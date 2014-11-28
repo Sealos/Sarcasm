be389b1415bf21881e3d236c37eec28db22c0ca6
Stefano De Colli <sdecolli@gmail.com>
2014-11-27 20:57:47 -0430

    Ya leemos a enteros

--- Proyecto Final/my_sarcasm_is_great.py
+++ Proyecto Final/my_sarcasm_is_great.py
@@ -17,3 +17,10 @@ with open('data.csv', 'r') as my_file:
 	for row in my_csv:
 		target.append(row[1])
 		data.append(row[2:])
+
+	# Transformamos la entrada a enteros
+
+	for i in range(len(target)):
+		target[i] = int(target[i])
+		for j in range(len(data[i])):
+			data[i][j] = int(data[i][j])
